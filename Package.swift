bc8cf42698743dfecb0ab1fb145a5d8b2f055634
Koray Koska <koray@koska.at>
2017-09-18 16:16:08 +0200

    Update to SPM v4 format

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
+// swift-tools-version:4.0
 import PackageDescription
 import Foundation
 
 let package = Package(
     name: "LibStorj",
-    targets: [
-        Target(name: "LibStorj")
+    products: [
+        .library(name: "LibStorj", targets: ["LibStorj"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/Ybrin/CLibStorj.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1, 0, 0)..<Version(3, .max, .max))
+        .package(url: "https://github.com/Ybrin/CLibStorj.git", from: "1.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "3.0.0"),
     ],
-    exclude: []
+    targets: [
+        .target(name: "LibStorj", dependencies: ["CLibStorj", "SwiftyJSON"]),
+        .testTarget(name: "LibStorjTests", dependencies: ["LibStorj"]),
+    ],
+    swiftLanguageVersions: [3]
 )
